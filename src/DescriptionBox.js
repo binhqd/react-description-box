60859df81e3c64fd23f94a6170a631076691caca
Binh Quan <binhqd@gmail.com>
2018-03-07 13:59:28 +0700

    Enable customizable description box container See #14

--- src/DescriptionBox.js
+++ src/DescriptionBox.js
@@ -1,13 +1,26 @@
 import React from 'react';
+import {PropTypes} from 'prop-types';
 import strShorten from 'str_shorten';
 
 class DescriptionBox extends React.Component {
 
   render() {
-    return (
-      <div>{strShorten(this.props.content, this.props.maxChars)}</div>
-    );
+    let {content, maxChars} = this.props;
+
+    return <this.props.component>{strShorten(content, maxChars)}</this.props.component>;
   }
 }
+
+DescriptionBox.propTypes = {
+  component: PropTypes.func
+};
+
+DescriptionBox.defaultProps = {
+  component: function(props) {
+    return (
+      <div>{props.children}</div>
+    );
+  }
+};
 
 export default DescriptionBox;
